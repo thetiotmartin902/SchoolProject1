d449204bb38d28da686227ced081db62ba999bc5
thetiotmartin902 <thetiotmartin902@gmail.com>
2025-03-13 14:42:50 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char* name = "John";
+    int age = 25;
+    printf("Hello, my name is %s and I am %d years old.\n", name, age);
+    return 0;
+}
