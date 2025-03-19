01600355671efc561b54223b42e3b72920c3a0c6
thetiotmartin902 <thetiotmartin902@gmail.com>
2025-03-19 21:26:35 +0000

    Add 14.c

---
+++ src/14.c
@@ -0,0 +1,10 @@
+
+int main() {
+  int x = 5;
+  if (x > 3) {
+    printf("The value of x is greater than 3");
+  } else {
+    printf("The value of x is less than or equal to 3");
+  }
+  return 0;
+}
